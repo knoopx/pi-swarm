f315efe9e5fc9c72b0b8cf8e0d267959b04d55c9
Victor Martinez <knoopx@gmail.com>
2026-02-09 11:46:15 +0100

    style(ui): 💄 add system font stack to tailwind config

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -4,6 +4,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
       colors: {
         // Base16 palette
         base00: "#191033",
